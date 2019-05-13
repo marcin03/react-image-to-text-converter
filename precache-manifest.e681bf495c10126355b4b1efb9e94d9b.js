bee917a57308b3915b39fc9fb6cb50475a52b692
marcin03 <marcinmulawa92@gmail.com>
2019-05-13 20:43:09 +0200

    Updates

---
+++ precache-manifest.e681bf495c10126355b4b1efb9e94d9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2d63079e1d89c3802ef8",
+    "url": "/static/js/main.6153acb5.chunk.js"
+  },
+  {
+    "revision": "05ce80e32466da69e28f",
+    "url": "/static/js/2.33205529.chunk.js"
+  },
+  {
+    "revision": "2d63079e1d89c3802ef8",
+    "url": "/static/css/main.6d2168cd.chunk.css"
+  },
+  {
+    "revision": "05ce80e32466da69e28f",
+    "url": "/static/css/2.d1a73c8a.chunk.css"
+  },
+  {
+    "revision": "2d492787fa9edde4f6ab8b3ff3e5149a",
+    "url": "/index.html"
+  }
+];
